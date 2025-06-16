58945e67581ebaf1cc80ef9a006a01baa7d42b59
Carlos Perez <98119612+charger17@users.noreply.github.com>
2025-06-16 11:10:21 -0600

    Update App.js

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,15 +3,22 @@ import { mount } from "marketing/MarketingApp";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <>
       <BrowserRouter>
-        <div>
-          <Header />
-          <MarketingApp />
-        </div>
+        <StylesProvider generateClassName={generateClassName}>
+          <div>
+            <Header />
+            <MarketingApp />
+          </div>
+        </StylesProvider>
       </BrowserRouter>
     </>
   );
