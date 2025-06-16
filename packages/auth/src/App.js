a9764bf221bc84aa3113ac136e9bb3e854151d71
Carlos Perez <98119612+charger17@users.noreply.github.com>
2025-06-16 17:05:24 -0600

    Update App.js

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
-import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
-import Signin from './components/SignIn';
-import Signup from './components/SignUp';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
 
 const generateClassName = createGenerateClassName({
-    productionPrefix: 'au'
+  productionPrefix: 'au',
 });
 
 export default ({ history, onSignIn }) => {
-    return (
-        <>
-            <StylesProvider generateClassName={generateClassName}>
-                <Router history={history}>
-                    <Switch>
-                        <Route path="/auth/signin">
-                            <Signin onSignIn={onSignIn} />
-                        </Route>
-                        <Route path="/auth/signup" component={Signin} />
-                    </Switch>
-                </Router>
-            </StylesProvider>
-        </>
-    )
-}
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+            <Route path="/auth/signin">
+              <Signin onSignIn={onSignIn} />
+            </Route>
+            <Route path="/auth/signup">
+              <Signup onSignIn={onSignIn} />
+            </Route>
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+};
