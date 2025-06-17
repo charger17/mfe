c04d19eb237bd77ce574657d732efe265c0de531
Carlos Perez <98119612+charger17@users.noreply.github.com>
2025-06-16 18:01:42 -0600

    Add files via upload

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,24 +1,37 @@
-import React, { lazy, Suspense, useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { lazy, Suspense, useState, useEffect } from "react";
+import { Router, BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import Header from "./components/Header";
 import Progress from "./components/Progress";
 
+
 import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+import { createBrowserHistory } from "history";
 
 const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 const AuthLazy = lazy(() => import("./components/AuthApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push("/dashboard");
+    } else {
+      history.push("/");
+    }
+  }, [isSignedIn]);
+
   return (
     <>
-      <BrowserRouter>
+      <Router history={history} >
         <StylesProvider generateClassName={generateClassName}>
           <div>
             <Header
@@ -30,6 +43,12 @@ export default () => {
                 <Route path="/auth">
                   <AuthLazy onSignIn={() => setIsSignedIn(true)} />
                 </Route>
+                <Route path="/dashboard">
+                  {
+                    !isSignedIn && <Redirect to="/" />
+                  }
+                  <DashboardLazy />
+                </Route>
                 <Route path="/">
                   <MarketingLazy isSignedIn={isSignedIn} />
                 </Route>
@@ -37,7 +56,7 @@ export default () => {
             </Suspense>
           </div>
         </StylesProvider>
-      </BrowserRouter>
+      </Router>
     </>
   );
 };
